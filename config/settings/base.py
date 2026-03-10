88059791bb93d0e60843535453c52c167431dbe4
Emilakesofficial <realemilakes@gmail.com>
2026-03-10 05:24:08 -0700

    fixed celery settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -314,8 +314,8 @@ WALLET_MAX_DIGITS = 20
 SYSTEM_WALLET_NAME = 'SYSTEM'
 
 # Celery Configuration
-CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
+CELERY_BROKER_URL = env('REDIS_URL')
+CELERY_RESULT_BACKEND = env('REDIS_URL')
 
 # Serialization
 CELERY_ACCEPT_CONTENT = ['json']
